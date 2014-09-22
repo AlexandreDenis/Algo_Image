3d028b4eefdfff82e36e03a60a1cdfbaf7af6005
Alexandre Denis <alexandre.denis@dbmail.com>
2014-09-22 19:22:33 +0200

    test envoi script init

---
+++ init_git.sh
@@ -0,0 +1,7 @@
+git init
+git config --global user.name "Alexandre Denis"
+git config --global user.email "alexandre.denis@dbmail.com"
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=3600'
+git remote add origin $1
+git pull origin master
